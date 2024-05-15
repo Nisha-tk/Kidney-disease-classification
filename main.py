f17630a2d65f908bfa66c2c8590054c1b59e78ed
Nisha-tk <nisha.333t@gmail.comgit config --global user.email nisha.333t@gmail.com>
2024-05-16 04:26:02 +0530

    "model Evaluation"

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from cnnClassifier import logger
 from cnnClassifier.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipelines.stage_02_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipelines.stage_03_model_training import ModelTrainingPipeline
+from cnnClassifier.pipelines.stage_04_model_evaluation import EvaluationPipeline
+import mlflow
+
 
 
 
@@ -36,6 +39,19 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
